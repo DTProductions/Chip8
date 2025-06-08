ec7d9cfee6817fb43f0e286f614d8f124e8702ef
Diogo Tosta Silva <diogotosta_silva@hotmail.com>
2025-06-08 18:45:25 -0300

    add command-line arguments

--- Program.cs
+++ Program.cs
@@ -1,10 +1,36 @@
 ﻿using SDL2;
 using System;
+using System.Timers;
 
 
 namespace Chip8Interpreter {
 	internal class Program {
+
 		static void Main(string[] args) {
+			if(args.Length < 1) {
+				return;
+			}
+
+			string instructionsPath = args[0];
+
+			bool useOriginalJumpWithOffset = true;
+			bool useOriginalShift = true;
+			bool useOriginalMemoryHandling = true;
+
+			for(int i = 1; i < args.Length; i++) {
+				switch (args[i]) {
+					case "alt-jump":
+						useOriginalJumpWithOffset = false;
+						break;
+					case "alt-shift":
+						useOriginalShift = false;
+						break;
+					case "alt-mem-handl":
+						useOriginalMemoryHandling = false;
+						break;
+				}
+			}
+
 			if (SDL.SDL_Init(SDL.SDL_INIT_VIDEO | SDL.SDL_INIT_AUDIO) < 0) {
 				throw new Exception(SDL.SDL_GetError());
 			}
@@ -13,7 +39,7 @@ namespace Chip8Interpreter {
 			SdlKeypad keypad = new SdlKeypad();
 			SdlAudioManager audioManager = new SdlAudioManager(220, 64);
 
-			Interpreter interpreter = new Interpreter(sdlDisplay, keypad, "slipperyslope.ch8", audioManager, true, true, true);
+			Interpreter interpreter = new Interpreter(sdlDisplay, keypad, instructionsPath, audioManager, useOriginalShift, useOriginalJumpWithOffset, useOriginalMemoryHandling);
 
 			Decoder decoder = new Decoder(interpreter);
 
